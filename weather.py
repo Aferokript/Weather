99512513f7011ee709802609d6350f09c8644d5e
Aferokript <alberto.cardinale@bk.ru>
2026-02-05 20:10:07 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,40 +1,43 @@
 import requests
 
-def get_weather_info():
-    dict_of_place = {
-        'Шерементево': 'https://wttr.in/SVO',
+def fetch_weather(url, params):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.text
+
+
+def get_weather_data():
+    locations = {
+        'Шереметьево': 'https://wttr.in/SVO',
         'Череповец': 'https://wttr.in/Череповец',
         'Лондон': 'https://wttr.in/Лондон'
     }
 
     params = {
         'lang': 'ru',
-        'm': '',
-        'M': '',
-        'q': '',
-        '0': ''
+        'm': '',  
+        'M': '',  
+        'q': '',  
+        '0': ''  
     }
 
-    weather_data = []
+    weather_data = {}
 
-    for name,url in dict_of_place.items():
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        weather_data.append(response.text)
+    for name, url in locations.items():
+        weather_text = fetch_weather(url, params)
+        weather_data[name] = weather_text
     return weather_data
 
-
-def print_weather_data():
-    weather_data = get_weather_info()
-    print(f'Погода в аэропорту Шерементево: {weather_data[0]}')
-    print(f'Погода в Череповце: {weather_data[1]}')
-    print(f'Погода в Лондоне: {weather_data[2]}')
+def print_weather(weather_data):
+    for name, weather_text in weather_data.items():
+        print(f'Погода в {name}: {weather_text}')
 
 
 def main():
-    print_weather_data()
-
+    weather_data = get_weather_data()
+    print_weather(weather_data)
 
 if __name__ == '__main__':
     main()
+
 
