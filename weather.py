c6480fabb39b7ac1ea798e73be479c5452dec4d8
Aferokript <alberto.cardinale@bk.ru>
2026-02-06 15:44:40 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,41 +1,28 @@
 import requests
 
-def fetch_weather(url, params):
+
+def fetch_weather(location, params):
+    url = f"https://wttr.in/{location}"
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.text
 
 
-def get_weather_data():
-    locations = {
-        'Шереметьево': 'https://wttr.in/SVO',
-        'Череповец': 'https://wttr.in/Череповец',
-        'Лондон': 'https://wttr.in/Лондон'
-    }
+def main():
+    locations = ['SVO', 'Череповец', 'Лондон']
 
     params = {
         'lang': 'ru',
-        'm': '',  
-        'M': '',  
-        'q': '',  
-        '0': ''  
+        'm': '',
+        'M': '',
+        'q': '',
+        '0': '' 
     }
 
-    weather_data = {}
+    for location in locations:
+        weather_text = fetch_weather(location, params)
+        print(weather_text)
 
-    for name, url in locations.items():
-        weather_text = fetch_weather(url, params)
-        weather_data[name] = weather_text
-    return weather_data
-
-def print_weather(weather_data):
-    for name, weather_text in weather_data.items():
-        print(f'Погода в {name}: {weather_text}')
-
-
-def main():
-    weather_data = get_weather_data()
-    print_weather(weather_data)
 
 if __name__ == '__main__':
     main()
